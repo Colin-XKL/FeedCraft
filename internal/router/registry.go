23009a81643e372690939cc731f7989493e75372
colin <Colin_XKL@outlook.com>
2024-05-02 16:46:38 +0800

    feat: add a web page to help user generate feed url

--- internal/router/registry.go
+++ internal/router/registry.go
@@ -2,10 +2,24 @@ package router
 
 import (
 	"FeedCraft/internal/recipe"
+	"FeedCraft/internal/util"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func RegisterRouters(router *gin.Engine) {
+	envClient := util.GetEnvClient()
+	if envClient == nil {
+		log.Fatalf("get env client error.")
+	}
+	siteBaseUrl := envClient.GetString("SITE_BASE_URL")
+	router.LoadHTMLFiles("web/index.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"SiteBaseUrl": siteBaseUrl,
+		})
+	})
 
 	// Public routes
 	public := router.Group("/craft")
