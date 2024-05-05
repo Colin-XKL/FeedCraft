5d1a6ddfcdbf660741fd27f09677691526d31f72
colin <Colin_XKL@outlook.com>
2024-05-05 16:17:12 +0800

    chore: remove stylelint and add code-inspector-plugin

--- web/admin/config/vite.config.dev.ts
+++ web/admin/config/vite.config.dev.ts
@@ -1,5 +1,6 @@
 import { mergeConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 import baseConfig from './vite.config.base';
 
 export default mergeConfig(
@@ -16,6 +17,9 @@ export default mergeConfig(
         cache: false,
         include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
         exclude: ['node_modules'],
+      }),
+      codeInspectorPlugin({
+        bundler: 'vite',
       }),
     ],
   },
