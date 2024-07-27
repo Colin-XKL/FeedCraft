7d568e3aa833d7999a8abba897594a382f91b409
colin <Colin_XKL@outlook.com>
2024-07-27 22:40:35 +0800

    feat: add feed compare page

---
+++ web/admin/src/views/dashboard/feed_compare/feed_compare.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <x-header
+      title="Feed Compare"
+      description="指定feed源url与要应用的craft(craft flow 或 atom都可), 对比处理前和处理后的结果"
+    ></x-header>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import XHeader from '@/components/header/x-header.vue';
+</script>
+
+<script lang="ts">
+  export default {
+    name: 'FeedCompare',
+  };
+</script>
