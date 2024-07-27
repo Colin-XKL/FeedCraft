883a02e57d5b49a9c10ce55ebfd6d38c8012d654
colin <Colin_XKL@outlook.com>
2024-07-27 18:25:41 +0800

    feat: add init logic for craft atom

--- internal/dao/migrate.go
+++ internal/dao/migrate.go
@@ -10,7 +10,7 @@ func MigrateDatabases() {
 	db := util.GetDatabase()
 	err := db.AutoMigrate(
 		&CustomRecipe{},
-		&CraftFlow{},
+		&CraftFlow{}, &CraftAtom{},
 	)
 	if err != nil {
 		logrus.Error("migrate database error.", err)
