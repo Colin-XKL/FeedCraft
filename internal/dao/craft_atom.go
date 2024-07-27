07a83bc109a756e155ce3b3e7fbbb67e93af9402
colin <Colin_XKL@outlook.com>
2024-07-27 17:42:43 +0800

    feat: add db curd

--- internal/dao/craft_atom.go
+++ internal/dao/craft_atom.go
@@ -1,5 +1,7 @@
 package dao
 
+import "gorm.io/gorm"
+
 // CraftAtom craft atom 由 craft template 派生出来
 type CraftAtom struct {
 	BaseModelWithPK
@@ -7,4 +9,37 @@ type CraftAtom struct {
 	Description  string            `json:"description"`
 	TemplateName string            `json:"template_name" binding:"required" `
 	Params       map[string]string `json:"params" gorm:"serializer:json" gorm:"index"`
+}
+
+// CreateCraftAtom creates a new CraftAtom record
+func CreateCraftAtom(db *gorm.DB, craftAtom *CraftAtom) error {
+	return db.Create(craftAtom).Error
+}
+
+// GetCraftAtomByName retrieves a CraftAtom record by its Name
+func GetCraftAtomByName(db *gorm.DB, name string) (*CraftAtom, error) {
+	var craftAtom CraftAtom
+	result := db.Where("name = ?", name).First(&craftAtom)
+	return &craftAtom, result.Error
+}
+
+// GetAllCraftAtoms retrieves all CraftAtom records
+func GetAllCraftAtoms(db *gorm.DB) ([]CraftAtom, error) {
+	var craftAtoms []CraftAtom
+	result := db.Find(&craftAtoms)
+	return craftAtoms, result.Error
+}
+
+// UpdateCraftAtom updates an existing CraftAtom record
+func UpdateCraftAtom(db *gorm.DB, craftAtom *CraftAtom) error {
+	return db.Save(craftAtom).Error
+}
+
+// DeleteCraftAtom deletes a CraftAtom record by its Name
+func DeleteCraftAtom(db *gorm.DB, name string) error {
+	craftAtom, err := GetCraftAtomByName(db, name)
+	if err != nil {
+		return err
+	}
+	return db.Delete(craftAtom).Error
 }
