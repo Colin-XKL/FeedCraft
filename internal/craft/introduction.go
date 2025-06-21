c55150e4ce209dda35bb42f26928219a2d39106f
colin <Colin_XKL@outlook.com>
2025-06-21 20:56:46 +0800

    feat: update llm prompt for generate article introduction text

--- internal/craft/introduction.go
+++ internal/craft/introduction.go
@@ -9,14 +9,29 @@ import (
 )
 
 /*
-add introduction for article,powered by google gemini
+add an introduction for article, using LLM power
 */
 
 func getIntroductionForArticle(prompt, article string) (string, error) {
 	return adapter.CallLLMUsingContext(prompt, article)
 }
 
-const promptGenerateIntroduction = "请阅读下面的文章并写一篇不超过200字的中文摘要, 使得读者可以快速知道文章的主题和主要结论."
+const promptGenerateIntroduction = `
+你是一位专业的文章导读撰写专家，擅长用简洁的语言吸引读者注意力并概括文章核心内容。请根据用户提供的文章内容，生成一段言简意赅、引人入胜的文章导读。导读需满足以下要求：
+
+吸引注意力：根据文章中的内容, 通过提问、引用数据、讲述故事或制造悬念等方式，激发读者的兴趣。
+概括核心：用1-2句话清晰传达文章的主题或核心观点。
+引导阅读：暗示文章的价值或结构，鼓励读者继续阅读。
+语言风格：简洁有力，避免冗长或复杂表达。
+
+输出要求：
+语言要求：使用简体中文
+字数限制：中文不超过120字
+语言风格：简洁、生动、引人入胜。口语化但专业，避免术语堆砌。
+
+请根据以下文章内容生成导读：
+
+`
 
 func combineIntroductionAndArticle(article, intro string) string {
 	introInHtml := util.Markdown2HTML(intro)
