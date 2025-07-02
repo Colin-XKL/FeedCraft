465305b181b0ae67877a1058bf35cbce73c12be8
colin <Colin_XKL@outlook.com>
2025-07-02 22:58:13 +0800

    feat: 添加LLM调用超时处理

--- internal/adapter/llm.go
+++ internal/adapter/llm.go
@@ -5,26 +5,6 @@ import (
 	"fmt"
 )
 
-// CallGeminiUsingArticleContext @deprecated
-func CallGeminiUsingArticleContext(prompt string, article string) (string, error) {
-	gemini, err := NewGeminiClient()
-	if err != nil {
-		return "", err
-	}
-	text := fmt.Sprintf("%s\n```%s```", prompt, article)
-	req := GeminiReqPayload{Contents: []Content{
-		{
-			Parts: []Part{{Text: &text}},
-			Role:  nil,
-		},
-	}}
-	content, err := gemini.GenerateContent(req)
-	if err != nil {
-		return "", err
-	}
-	return content, nil
-}
-
 // CallLLMUsingContext using openai compatible api
 func CallLLMUsingContext(prompt, context string) (string, error) {
 	finalPrompt := fmt.Sprintf("%s \n```\n%s\n```", prompt, context)
