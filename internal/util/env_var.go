7dfea24e1a56ad139f403a7494e11bf6b2c18110
gifwang <gifwang@tencent.com>
2026-04-14 15:20:03 +0800

    chore: 新增忽略 start.sh 与 start-full.sh 文件

--- internal/util/env_var.go
+++ internal/util/env_var.go
@@ -2,9 +2,13 @@ package util
 
 import (
 	"github.com/spf13/viper"
+	"github.com/subosito/gotenv"
 )
 
 func GetEnvClient() *viper.Viper {
+	// Load .env file if it exists
+	gotenv.Load()
+
 	v := viper.New()
 
 	// Set the name of the environment variable prefix (optional)
