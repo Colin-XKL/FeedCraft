c13f529bf93f408a05db135088983f24502f7217
Colin <Colin_XKL@outlook.com>
2026-05-10 18:26:57 +0800

    feat: fix i18n for zh-TW

--- web/admin/src/locale/index.ts
+++ web/admin/src/locale/index.ts
@@ -1,9 +1,11 @@
 import { createI18n } from 'vue-i18n';
 import en from '@/locale/en-US';
 import cn from '@/locale/zh-CN';
+import tw from '@/locale/zh-TW';
 
 export const LOCALE_OPTIONS = [
   { label: '中文', value: 'zh-CN' },
+  { label: '繁體中文', value: 'zh-TW' },
   { label: 'English', value: 'en-US' },
 ];
 const defaultLocale = localStorage.getItem('arco-locale') || 'zh-CN';
@@ -16,6 +18,7 @@ const i18n = createI18n({
   messages: {
     'en-US': en,
     'zh-CN': cn,
+    'zh-TW': tw,
   },
 });
 
